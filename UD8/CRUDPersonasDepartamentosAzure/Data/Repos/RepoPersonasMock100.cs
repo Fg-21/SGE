7884fa3fc9c11d901e08ea5ac9203beb559c570d
Fg-21 <fm.gonzalez@iesnervion.es>
2026-01-12 14:41:50 +0100

    asda

---
+++ UD8/CRUDPersonasDepartamentosAzure/Data/Repos/RepoPersonasMock100.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repos
+{
+    public class RepoPersonasMock100 : IRepoPersonas
+    {
+        public int createPersona(Persona newPersona)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int deletePersona(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Persona[] getListaPersonas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Persona> getLISTAPersonas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Persona getPersonaById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int updatePersona(int id, Persona ePersona)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
